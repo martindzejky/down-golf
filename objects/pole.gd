f99082c72d8b062b612aaeef2d149a138598d565
Martin Jakubik <martindzejky@gmail.com>
2025-04-05 17:23:59 +0200

    fix detection

--- objects/pole.gd
+++ objects/pole.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 signal ball_entered_hole
 
-
-func _on_balldetect_area_entered(area: Area2D) -> void:
-  if area.is_in_group('ball'):
+func _on_balldetect_body_entered(body: Node2D) -> void:
+  if body.is_in_group('ball'):
     ball_entered_hole.emit()
