df587a4e5d5bba023a5c84e31acb23c6b889cd63
Martin Jakubik <martindzejky@gmail.com>
2025-04-06 11:12:22 +0200

    pole player blocker

--- objects/pole.gd
+++ objects/pole.gd
@@ -1,7 +1,16 @@
 extends Node2D
 
+@export var is_done: bool = false
+@export var player_blocker: Node2D
+
 signal ball_entered_hole
+signal player_entered_pole
 
 func _on_balldetect_body_entered(body: Node2D) -> void:
   if body.is_in_group('ball'):
     ball_entered_hole.emit()
+    is_done = true
+    player_blocker.queue_free()
+
+  if body.is_in_group('player'):
+    player_entered_pole.emit()
