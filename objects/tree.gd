7e3f9fb444d58c2a1b14ab4f99695cf389faf965
Martin Jakubik <martindzejky@gmail.com>
2025-04-06 17:58:40 +0200

    leaves

--- objects/tree.gd
+++ objects/tree.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 @export var animation: AnimationPlayer
+@export var particles: GPUParticles2D
 
 func _on_area_body_entered(body: Node2D) -> void:
   if body.is_in_group('ball') and not animation.is_playing():
     animation.play('shake')
+    particles.emitting = true
